4c4295c79dfb7d306fda2b01b012a65888f317de
Joseph Hill <hello@josephh.dev>
2025-11-03 21:07:54 +0000

    Bank account class

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -2,6 +2,30 @@
 
 namespace BankingApp
 {
+    class BankAccount
+    {
+        private string Username;
+        private string Password;
+        private double AccountBalance = 0;
+        public BankAccount(string InitialUsername, string InitialPassword)
+        {
+            Username = InitialUsername;
+            Password = InitialPassword;
+        }
+        public bool Authenticate(string _Password) 
+        {
+            return (Password == _Password);
+        }
+        public double GetAccountBalance()
+        {
+            return AccountBalance;
+        }
+        public double ModifyAccountBalance(double ModifyBy)
+        {
+            AccountBalance += ModifyBy;
+            return AccountBalance;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
